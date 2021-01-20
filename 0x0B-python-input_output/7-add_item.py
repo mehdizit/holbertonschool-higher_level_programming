f62983f3dfdab49fc5df904666535d8ee61956e7
mehdizit <zitounimehdi.it@gmail.com>
2021-01-20 23:43:30 +0100

    up

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""
-adds all arguments to a Python list, and then save them to a file
-"""
+""" module """
+import json
 import sys
+dump = __import__("5-save_to_json_file").save_to_json_file
+load = __import__("6-load_from_json_file").load_from_json_file
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+def main(argv):
+    """ create a json file from agumets given to the program """
+    try:
+        new = load("add_item.json")
+    except:
+        with open("add_item.json", mode="a") as file:
+            new = []
+    for el in sys.argv[1:]:
+        new.append(el)
+    dump(new, "add_item.json")
 
-open("add_item.json", "a")
-try:
-    l = load_from_json_file("add_item.json")
-except ValueError:
-    l = []
-save_to_json_file(l + sys.argv[1:], "add_item.json")
+
+if __name__ == "__main__":
+    main(sys.argv)
