407357076948aec78f14a9dbb29c6f0099117de3
mehdizit <zitounimehdi.it@gmail.com>
2021-01-23 21:31:33 +0100

    up

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """Defines a base model class."""
+
+
 class Base:
     """Represent the base model.
     Represents the "base" for all other classes in project 0x0C*.
     Attributes:
         __nb_objects (int): The number of instantiated Bases.
     """
+
     __nb_objects = 0
+
     def __init__(self, id=None):
-         """Initialize a new Base.
+        """Initialize a new Base.
         Args:
             id (int): The identity of the new Base.
         """
@@ -17,4 +21,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-            
