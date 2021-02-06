ce77b9342d4b4117ee14c266dc2b5faa8c2f7c2e
mehdizit <zitounimehdi.it@gmail.com>
2021-02-06 22:12:24 +0100

    up

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    """print integer"""
+
+    import sys
+
+    try:
+        print("{:d}".format(value))
+        return(True)
+    except (ValueError, TypeError) as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return(False)
+
+
+
