6533978d824b7386c893d5e0d5531ac487b36690
mehdizit <zitounimehdi.it@gmail.com>
2021-03-01 23:09:46 +0100

    up

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- Displays the number of records with id = 89 in the table first_table.
-SELECT COUNT(id) FROM `first_table` where `id` = 89;
+-- Displays the number of records with id = 89 in the table first_table
+SELECT COUNT(id) FROM first_table Where id = 89;
