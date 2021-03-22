649f804fccabe58c3941baaace713296c1d1a01b
mehdizit <zitounimehdi.it@gmail.com>
2021-03-22 20:17:59 +0100

    up

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const cStatement = 'C is fun';
+const pStatement = 'Python is cool';
+const jStatement = 'Javascript is amazing';
+console.log(cStatement);
+console.log(pStatement);
+console.log(jStatement);
