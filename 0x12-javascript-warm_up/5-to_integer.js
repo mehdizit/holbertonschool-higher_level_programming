e912267a712427a155aa63a1ff74191c42fa58ac
mehdizit <zitounimehdi.it@gmail.com>
2021-03-22 20:51:11 +0100

    up

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const arg1 = process.argv[2];
+const integer = parseInt(arg1, 10);
+if (isNaN(integer)) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', integer);
+}
