ae54f4b6201364881caa093c14a076870ee5ccbf
mehdizit <zitounimehdi.it@gmail.com>
2021-03-28 22:02:18 +0100

    up

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,13 @@
 #!/usr/bin/python3
-"""lists all states from database hbtn_0e_0_usa starting with N"""
+"""Displays all values in the states table of the database hbtn_0e_0_usa"""
+
 import sys
 import MySQLdb
 
-
-def main(argv):
-
-    if len(argv) - 1 != 4:
-        print("Must enter 4 arguments")
-        return
-
-    db = MySQLdb.connect(host="localhost",
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
-                         port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
-
-    for row in cur.fetchall():
-        if row[1] == argv[4]:
-            print(row)
-
-    db.close()
-
-
 if __name__ == "__main__":
-    import sys
-    main(sys.argv)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
