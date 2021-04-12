e0657b504f6c98651a78070e3bfeb8eb9761e245
mehdizit <zitounimehdi.it@gmail.com>
2021-04-12 22:14:22 +0100

    up

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''fetches X-request ID of https://intranet.hbtn.io/status'''
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+         html = response.getheader('X-Request-ID')
+         print(html)
